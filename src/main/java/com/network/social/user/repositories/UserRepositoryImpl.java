ad0c3878dc9d0e7a6c480373b132e35d26c6dc41
sidgharge <ghargesiddharth@gmail.com>
2018-08-21 21:11:45 +0530

    before pull

--- src/main/java/com/network/social/user/repositories/UserRepositoryImpl.java
+++ src/main/java/com/network/social/user/repositories/UserRepositoryImpl.java
@@ -1,9 +1,5 @@
 package com.network.social.user.repositories;
 
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +10,6 @@ import com.network.social.user.models.User;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-//	@PersistenceUnit
-//    private EntityManagerFactory emf;
-
 	@Autowired
 	private SessionFactory factory;
     
@@ -26,13 +19,4 @@ public class UserRepositoryImpl implements UserRepository {
 		session.save(user);
 	}
 	
-
-	//@PostConstruct
-//    private void setSessionFactory() {
-//        if (emf.unwrap(SessionFactory.class) == null) {
-//            throw new NullPointerException("factory is not a hibernate factory");
-//        }
-//        factory = emf.unwrap(SessionFactory.class);
-//    }
-
 }
